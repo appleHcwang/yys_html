9a02d01db91085100fd476ab7371bcbb099ca879
hcwang18 <hcwang18@iflytek.com>
2022-03-10 00:26:14 +0800

    feat: 项目配置

---
+++ yysClient/src/components/views/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+      <div class="login-titcnt">
+ sfsff
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='scss'>
+.login {
+}
+
+</style>
