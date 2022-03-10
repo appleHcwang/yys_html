70cfd90f96aa730aa62022206e986f0ec8060681
hcwang18 <hcwang18@iflytek.com>
2022-03-11 00:03:46 +0800

    feat: 登陆页

---
+++ yysClient/src/components/views/login.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="login">
+    <div class="login-top">
+      <img class="login-icon" src="@/assets/appicon.png" alt="" />
+      <p class="app-tle">讯飞云医声</p>
+      <div class="top-bt">
+        <span style="font-size: 18px; line-height: 18px">iDoctor</span
+        ><span style="font-size: 14px; margin-left: 10px; line-height: 18px; height: 18px;"
+          >临床工作好助手</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.login {
+  color: #333333;
+  display: flex;
+  &-top {
+    display: flex;
+    flex-direction: column;
+    font-size: 36px;
+    margin-left: 40px;
+    margin-top: 128px;
+    .app-tle {
+      line-height: 50px;
+      margin-top: 10px;
+      font-weight: 500;
+    }
+    .login-icon {
+      width: 78px;
+      height: 78px;
+      left: 0px;
+    }
+    .top-bt {
+      display: flex;
+      align-items: center;
+    }
+  }
+}
+</style>
