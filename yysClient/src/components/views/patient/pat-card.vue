89fe0626c298e6c80613b80103394eca8e8829d6
hcwang18 <hcwang18@iflytek.com>
2022-03-24 00:13:40 +0800

    feat: 患者卡片

--- yysClient/src/components/views/patient/pat-card.vue
+++ yysClient/src/components/views/patient/pat-card.vue
@@ -1,9 +1,39 @@
 <template>
   <div class="pat-card">
-    <p>123</p>
-    <p>456</p>
-    <p>789</p>
-    <p>10111112</p>
+   <p style="  background-color: #F2F2F2;
+  height: 6px;"></p>
+  <div class="pat-cardcnt">
+   
+    <p>
+      <span style="
+font-size: 16px;"> 1001床 </span>
+    </p>
+    <p>
+      <span style=" display: block;padding:10px 0px 6px 0px;"> 杨*桃 女 56岁 </span>
+    </p>
+    <p>
+      <span style="display: block;color: #999999; padding-bottom: 6px;"> 诊断：十二指肠癌切除术后伴肝脏、腹膜后多 诊断：十二指肠癌切除术后伴肝脏、腹膜后多 </span>
+    </p>
+
+    <p class="pat-cntbtm">
+      <span>
+        <img src="@/assets/yys/保险类型图标.png" alt="" />
+        <span style="margin-left: 4px;"> 季琳琳 </span>
+      </span>
+
+      <span style=" margin-left: 14px ;">
+        <img src="@/assets/yys/保险类型图标.png" alt="" />
+        <span style="margin-left: 4px;"> 季琳琳 </span>
+      </span>
+
+      <span style="margin-left: 14px;">
+        <img src="@/assets/yys/保险类型图标.png" alt="" />
+        <span style="margin-left: 4px;"> 2018-09-03 15:09 </span>
+      </span>
+    </p>
+  </div>
+
+
   </div>
 </template>
 <script>
@@ -27,4 +57,30 @@ export default {
 </script>
 
 <style lang="scss">
+.pat-card {
+  .pat-cardcnt {
+    padding:16px 16px;
+    font-weight: 500;
+    font-size: 14px;
+   
+   
+
+    .pat-cntbtm {
+      display: flex;
+      font-size: 12px;
+      align-items: center;
+      span {
+        display: flex;
+        align-items: center;
+        // text-align: center;
+        line-height: 12px;
+        height: 12px;
+       
+      }
+     
+      
+    }
+  }
+
+}
 </style>
