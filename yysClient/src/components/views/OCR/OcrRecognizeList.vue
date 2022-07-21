741c0850662ef782c65753c643e2731582df543d
hcwang18 <hcwang18@iflytek.com>
2022-07-21 23:48:40 +0800

    feat: 添加ocr

---
+++ yysClient/src/components/views/OCR/OcrRecognizeList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="ocr-recognize-list">
+ ocr 识别
+  </div>
+</template>
+<script>
+
+export default {
+  name:'OcrRecognizeList',
+  components:{ 
+
+  },
+  data(){
+    return {
+      options:[
+        {
+          name:'院内病历',
+          label:'院内病历'
+        },
+        {
+          name:'语音病历',
+          label:'语音病历'
+        },
+      ],
+      hospitalRecords:[],//院内病历
+      voiceRecords:[],// 语音病历
+      loading:false,
+      finished:false,
+      pageNum:'1'
+    }
+  },
+  created(){
+
+    // this.loadCaseList();
+
+  },
+  mounted(){
+
+
+  },
+  methods:{
+
+
+  }
+}
+</script>
+<style lang="scss">
+.ocr-recognize-list{
+
+
+
+}
+</style>
