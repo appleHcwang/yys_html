dfcbaf5281b2315b4b0d48ad4b1a3b94deffd88e
DESKTOP-JCIIHC7\王汉春 <2372233634@qq.com>
2022-10-21 14:54:13 +0800

    feat：路由拦截

--- yysClient/src/no-permission-interceptor.js
+++ yysClient/src/no-permission-interceptor.js
@@ -1,11 +1,16 @@
 import router from './components/router/index';
 
-
 router.beforeEach((to, from, next) => {
-   if(!localStorage.getItem('userInfo') && to.name !== 'login') {
-    next({name: 'login'})
+   console.log('名字')
+   console.log(to.name)
+   console.log('路径')
+   console.log(to.path)
+  const isLogin = localStorage.getItem('userInfo')?true:false
+   if(isLogin) {
+   //  next()
+    to.path === '/' ? next('/mainTabbar') : next()
    } else {
-      next()
+    to.path === '/' ? next() : next('/')
    }
   })
 
